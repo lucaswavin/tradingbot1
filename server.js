f1cd5977ec18a12a026fbe7c3280d8ae08648481
lucaswavin <lucas@wavinai.com>
2025-07-28 19:16:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.json());
