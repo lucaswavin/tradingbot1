05878ce470f4b492b6e5b969a785335c6de3e29d
lucaswavin <lucas@wavinai.com>
2025-07-29 20:33:26 +0200

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const webhookController = require('../controllers/webhookController');
 
-router.post('/', webhookController.handleWebhook);
+router.post('/', webhookController.handleTradingViewWebhook);
 
 module.exports = router;
